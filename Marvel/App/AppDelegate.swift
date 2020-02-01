391a7cd1443281ed5655b9651a81225892980adc
Marco Domingues <marco.domingues@wingman.pt>
2020-02-01 10:51:30 +0000

    Project setup

---
+++ Marvel/App/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Marvel
+//
+//  Created by Marco Domingues on 31/01/2020.
+//  Copyright © 2020 Marco. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+
+}
---
+++ Marvel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Marvel
+//
+//  Created by Marco Domingues on 31/01/2020.
+//  Copyright © 2020 Marco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .orange
+    }
+
+}
